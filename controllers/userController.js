775dd45492e70dbc0825c344049609c9f440bcb1
vansharorx <vansharora21102005@gmail.com>
2026-03-30 19:01:09 +0530

    feat: add input validation for user API

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,22 @@
+const { validationResult } = require('express-validator');
+
 exports.getUsers = (req, res) => {
-    res.json({
-        message: "Get all users"
-    });
+    res.json({ message: "Get all users" });
 };
 
 exports.createUser = (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            errors: errors.array()
+        });
+    }
+
     const { name, email } = req.body;
 
     res.json({
-        message: "User created",
+        message: "User created successfully",
         data: { name, email }
     });
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,17 @@ const router = express.Router();
 
 const { getUsers, createUser } = require('../controllers/userController');
 
+const { body } = require('express-validator');
+
 router.get('/', getUsers);
-router.post('/', createUser);
+
+router.post(
+    '/',
+    [
+        body('name').notEmpty().withMessage('Name is required'),
+        body('email').isEmail().withMessage('Valid email required')
+    ],
+    createUser
+);
 
 module.exports = router;
