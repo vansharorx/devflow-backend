bfa4a603b51414604a67573973b2cf297d497bbb
vansharorx <vansharora21102005@gmail.com>
2026-05-12 07:17:13 +0530

    feat: add dashboard analytics API

--- server.js
+++ server.js
@@ -14,10 +14,11 @@ const userRoutes = require('./routes/userRoutes');
 const projectRoutes = require('./routes/projectRoutes');
 const issueRoutes = require('./routes/issueRoutes');
 const errorHandler = require('./middleware/errorMiddleware');
-
+const dashboardRoutes = require('./routes/dashboardRoutes');
 app.use('/api/users', userRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api/issues', issueRoutes);
+app.use('/api/dashboard', dashboardRoutes);
 app.get('/health', (req, res) => {
     res.json({ message: "Server running" });
 });
