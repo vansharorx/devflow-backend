ed2b0cbdb8adecc6e3a04acb6cc5c1bd99ca9f80
vansharorx <vansharora21102005@gmail.com>
2026-05-16 15:49:09 +0530

    feat: implement issue comments and collaboration system

---
+++ controllers/commentController.js
@@ -0,0 +1,48 @@
+const {
+    createCommentService,
+    getIssueCommentsService
+} = require('../services/commentService');
+
+exports.createComment = async (req, res) => {
+    try {
+        const { issueId, comment } = req.body;
+
+        const newComment = await createCommentService({
+            issueId,
+            userId: req.user.id,
+            comment
+        });
+
+        res.json({
+            success: true,
+            message: "Comment added",
+            data: newComment
+        });
+
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+};
+
+exports.getComments = async (req, res) => {
+    try {
+        const { issueId } = req.params;
+
+        const comments =
+            await getIssueCommentsService(issueId);
+
+        res.json({
+            success: true,
+            data: comments
+        });
+
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+};
